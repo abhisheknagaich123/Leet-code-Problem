5cef664890de58a03b8f4a67a1d97518a773fced
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2022-08-03 21:46:10 +0530

    commit

--- src/leetcode.java
+++ src/leetcode.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class leetcode {
     public static void main(String[] args) {
-        System.out.println(" Hello Leet code ");
+        int arr[]={1,1,0,0,0,2,5,6};
+        int count=0;
+        int sum=0;
+        int add=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==1){
+                count++;
+            }
+            if (arr[i]==0){
+                sum++;
+            }
+            if(arr[i]>=1){
+               add+=arr[i];
+            }
+        }
+        System.out.println(count);
+        System.out.println(sum);
+        System.out.println(add);
     }
 }
