f9ed50473652493de7473a698fe37849e106450a
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-07 21:30:56 +0530

    commit

---
+++ src/majorityElement169.java
@@ -0,0 +1,20 @@
+public class majorityElement169 {
+    public int majorityElement(int[] nums) {
+        int res=nums[0];
+        int count =1;
+        for(int i=0; i<nums.length; i++){
+            int num=nums[i];
+            if(res==num){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count ==0){
+                res=num;
+                count=1;
+            }
+        }
+        return res;
+    }
+}
