14d77062ba1b86c83fae1af16829684297f2bffe
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-12 22:08:07 +0530

    commit

---
+++ src/inser_delete_380.java
@@ -0,0 +1,24 @@
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public class inser_delete_380 {
+//    HashMap<Integer,Integer>res;
+//    List<Integer>list;
+//
+//    public RandomizedSet() {
+//
+//
+//    }
+//
+//    public boolean insert(int val) {
+//
+//    }
+//
+//    public boolean remove(int val) {
+//
+//    }
+//
+//    public int getRandom() {
+//
+//    }
+//}
--- src/productExceptSelf238.java
+++ src/productExceptSelf238.java
@@ -1,22 +1,35 @@
 public class productExceptSelf238 {
-    public int[] productExceptSelf(int[] nums) {
-        int []left=new int[nums.length];
-        int right[]=new  int [nums.length];
-        left[0]=1;
-        for (int i = 1; i < nums.length ; i++) {
-            left[i]=left[i-1] *nums[i-1];
-        }
-        right[nums.length-1]=1;
-        for (int i = nums.length; i >-1 ; i--) {
-            right[i]=right[i+1] * right[i+1];
+//    public int[] productExceptSelf(int[] nums) {
+//        int []left=new int[nums.length];
+//        int right[]=new  int [nums.length];
+//        left[0]=1;
+//        for (int i = 1; i < nums.length ; i++) {
+//            left[i]=left[i-1] *nums[i-1];
+//        }
+//        right[nums.length-1]=1;
+//        for (int i = nums.length; i >-1 ; i--) {
+//            right[i]=right[i+1] * right[i+1];
+//        }
+//        int [] ans = new int [nums.length];
+//        for (int i = 0; i < nums.length ; i++) {
+//            ans[i]=left[i]*nums[i];
+//        }
+//return ans;
+//    }
+    public static void main(String[] args) {
+        int []nums = {1,2,3,4};
+        int sum=1;
+        for (int i = 0; i <nums.length ; i++) {
+            sum*=nums[i];
         }
         int [] ans = new int [nums.length];
-        for (int i = 0; i < nums.length ; i++) {
-            ans[i]=left[i]*nums[i];
+        for (int i = 0; i <nums.length ; i++) {
+            ans[i]=sum/nums[i];
         }
-return ans;
-    }
-    public static void main(String[] args) {
+        for (int i = 0; i < ans.length ; i++) {
+            System.out.print(ans[i]);
+        }
+
 
 
     }
