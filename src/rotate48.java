741c33456aef3e616c93b441a9f1d56bd5afe8df
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-14 23:04:18 +0530

    commit

--- src/rotate48.java
+++ src/rotate48.java
@@ -1,2 +1,24 @@
 public class rotate48 {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+
+
+        //    transope
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j <n ; j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n/2 ; j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][j-1-j];
+                matrix[i][j-1-j]=temp;
+            }
+        }
+
+
+    }
 }
