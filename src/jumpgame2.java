27d9df92c485ef13f0df628c9692d863a52ade33
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-18 22:23:48 +0530

    commit

---
+++ src/jumpgame2.java
@@ -0,0 +1,16 @@
+public class jumpgame2 {
+    public int jump(int[] nums) {
+        int jump=0;
+        int pos=0;
+        int des=0;
+        for (int i = 0; i < nums.length-1; i++) {
+            des=Math.max(des,nums[i]+i);
+            if (pos==i){
+                pos=des;
+                jump++;
+            }
+        }
+        return jump;
+
+    }
+}
