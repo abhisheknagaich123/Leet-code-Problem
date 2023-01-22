3726f7ebad8093edf29a6783300630cb100204d2
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-22 22:13:45 +0530

    commit

--- src/Leetcode_1_Two_sum.java
+++ src/Leetcode_1_Two_sum.java
@@ -1,11 +1,11 @@
 public class Leetcode_1_Two_sum {
 
     public static void main(String[] args) {
-        int arr[]={2,7,11,15};
+        int arr1[]={2,7,11,15};
         int target=9;
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = 0; i < arr1.length ; i++) {
 
-            if (arr[i]+arr[i+1]==target){
+            if (arr1[i]+arr1[i+1]==target){
                 System.out.println(i+" "+i+1);
                 break;
             }
