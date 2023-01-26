cfb09d2fcda7bef683901a5ede55dd8a2eef19c9
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-26 22:14:29 +0530

    commit

---
+++ src/isPerfectSquare367.java
@@ -0,0 +1,28 @@
+public class isPerfectSquare367 {
+    public static void main(String[] args) {
+        int num=36;
+//        double n = Math.sqrt(num);
+//        long a= n/num;
+
+//        System.out.println(n);
+//        if(n/(int)n==1)
+//            System.out.println("true");
+//        else System.out.println("false");
+
+        long start = 1;
+        long end = num;
+        while(start <= end){
+            long mid = (start + end)/2;
+            if(mid * mid == num){
+                System.out.println(mid);
+            }
+            else if(mid * mid > num){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+
+    }
+}
