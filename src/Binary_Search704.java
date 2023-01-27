2be67db842908557c5b9b9be442eecbc123602f0
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-27 20:30:58 +0530

    commit

---
+++ src/Binary_Search704.java
@@ -0,0 +1,23 @@
+public class Binary_Search704 {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
