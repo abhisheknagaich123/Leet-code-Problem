48c8e78f4d7370473ff7b79a3a2c100b30f97e80
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-28 19:41:46 +0530

    commit

---
+++ src/myAtoi8.java
@@ -0,0 +1,7 @@
+public class myAtoi8 {
+    public static void main(String[] args) {
+        String s = "4193 with words";
+        int num=Integer.valueOf(s);
+        System.out.println(num);
+    }
+}
