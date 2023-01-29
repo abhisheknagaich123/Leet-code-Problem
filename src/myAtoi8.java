198cff93bae93641786dc4f9e2611d3efa761d50
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-01-29 19:12:15 +0530

    commit

--- src/myAtoi8.java
+++ src/myAtoi8.java
@@ -1,7 +1,21 @@
 public class myAtoi8 {
     public static void main(String[] args) {
-        String s = "4193 with words";
-        int num=Integer.valueOf(s);
-        System.out.println(num);
+        int [] arr = {2,0,2,1,1,0};
+        int temp =0;
+       int n= arr.length;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            } }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
+
