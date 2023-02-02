bdc9b5f0977e3d6aa1a7b48ee6e11e48884209ae
Abhishek Nagaich <abhishek.nagaich_csd18@gla.ac.in>
2023-02-02 21:03:31 +0530

    commit

---
+++ src/middleNode876.java
@@ -0,0 +1,13 @@
+public class middleNode876 {
+    public static void main(String[] args) {
+//        if(head==null || head.next==null) return head;
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        while(fast!=null && fast.next!=null){
+//            slow=slow.next;
+//            fast=fast.next.next;
+//        }
+//        return slow;
+//    }
+    }
+}
